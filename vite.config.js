38e417035b950f96f1ca2d63ffcb8c1522e0659a
mkeller <7525285+keller-mark@users.noreply.github.com>
2022-10-19 09:41:55 -0500

    Update

---
+++ vite.config.js
@@ -0,0 +1,43 @@
+import react from '@vitejs/plugin-react';
+import svgr from 'vite-plugin-svgr';
+import { defineConfig } from 'vite';
+import { resolve } from 'path';
+
+const isProduction = process.env.APP_ENV === 'production';
+
+// For tests.
+export default defineConfig({
+  build: {
+    sourcemap: true,
+    emptyOutDir: false,
+    minify: isProduction ? 'esbuild' : false,
+    lib: {
+      entry: resolve(__dirname, 'src/index.js'),
+      name: 'reactcolor',
+      fileName: isProduction ? 'index.min' : 'index',
+      formats: isProduction ? ['umd'] : ['es', 'umd'],
+    },
+    rollupOptions: {
+      external: ['react', 'react-dom'],
+      output: {
+        globals: {
+          react: 'React',
+          'react-dom': 'ReactDOM',
+        },
+      }
+    }
+  },
+  plugins: [
+    react({
+      jsxRuntime: 'classic',
+    }),
+    svgr(),
+  ],
+  esbuild: {
+    loader: 'jsx',
+    include: /src\/.*\.jsx?$/,
+    // loader: "tsx",
+    // include: /src\/.*\.[tj]sx?$/,
+    exclude: [],
+  },
+});
